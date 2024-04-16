e1253d90756ecb6930e9eac8e1041d42ac669477
Sasha dev <boltbright525@gmail.com>
2024-04-16 19:47:20 -0300

    feat: footer blocks

---
+++ src/components/ui/title.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ReactNode } from 'react';
+
+export type TitleLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+export type TitleSize = 'small' | 'medium' | 'large' | 'x_large';
+
+export interface TitleProps extends React.ComponentProps<'h1'> {
+  level?: TitleLevel;
+  size?: TitleSize;
+  children?: ReactNode;
+}
+
+export function Title({
+  level = 'h1',
+  size = 'x_large',
+  children,
+  ...props
+}: TitleProps) {
+  let headingClasses = 'font-primary';
+
+  switch (size) {
+    case 'small':
+      headingClasses += ' text-2xl';
+      break;
+    case 'medium':
+      headingClasses += ' text-4xl';
+      break;
+    case 'large':
+        headingClasses += ' text-6xl';
+        break;
+    default:
+      headingClasses += ' text-8xl';
+      break;
+  }
+
+  props.className = props.className
+    ? headingClasses + ' ' + props.className
+    : headingClasses;
+
+  return React.createElement(level, { ...props }, children);
+}
---
+++ src/components/ui/wrapper.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export interface WrapperProps extends React.ComponentProps<"div"> {
+  children?: ReactNode;
+}
+
+export default function Wrapper({ children, ...props }: WrapperProps) {
+  let divClasses = "xl:w-[1024px] 2xl:w-[1280px] mx-auto";
+  props.className = props.className
+    ? divClasses + " " + props.className
+    : divClasses;
+  return <div {...props}>{children}</div>;
+}
