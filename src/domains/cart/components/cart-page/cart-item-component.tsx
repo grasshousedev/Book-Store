8db564a83516c276c4757820f76db2ad0e181501
Sasha dev <boltbright525@gmail.com>
2024-05-08 11:53:58 -0300

    feat: adjustments to cart items grid

--- src/domains/cart/components/cart-page/cart-item-component.tsx
+++ src/domains/cart/components/cart-page/cart-item-component.tsx
@@ -8,7 +8,9 @@ import { ProductWithPageAndBookPrisma } from "@/domains/product/types/product-pr
 import { ProductLinkComponent } from "@/domains/product/components/product-link-component";
 import { getAuthorsByProduct } from "@/domains/product/helpers/get-authors-by-product";
 import { UiLoaderComponent } from "@/domains/ui/components/ui-loader-component";
-import { ChevronDownIcon } from "lucide-react";
+import { MinusIcon, PlusIcon } from "lucide-react";
+import { useCartContext } from "../../contexts/cart-context";
+import { CartActionTypes } from "../../enums/cart-action-types";
 
 async function fetchProduct(id: number): Promise<ProductWithPageAndBookPrisma> {
   const response = await axios.get(`/api/product/${id}`);
@@ -16,6 +18,8 @@ async function fetchProduct(id: number): Promise<ProductWithPageAndBookPrisma> {
 }
 
 export function CartItemComponent({ cartItem }: { cartItem: CartItemType }) {
+  const cartDispatch = useCartContext().dispatch;
+
   const response = useQuery({
     queryKey: ["product", cartItem.id],
     queryFn: () => fetchProduct(cartItem.id),
@@ -28,29 +32,65 @@ export function CartItemComponent({ cartItem }: { cartItem: CartItemType }) {
   const product = response.data;
 
   return (
-    <li className="grid grid-cols-[145px_calc(100%-145px-16px)] lg:grid-cols-[145px_calc(100%-145px-32px-30%)_30%] py-4 border-b gap-4">
-      <ProductLinkComponent product={product} className="block w-[145px]">
+    <li className="py-4 border-b grid grid-cols-[145px_calc(100%-145px-16px)] gap-4">
+      <ProductLinkComponent
+        product={product}
+        className="w-[145px] lg:row-span-2"
+      >
         <div className="bg-gray-500 w-[145px] h-[193px]"></div>
       </ProductLinkComponent>
-      <div className="">
+      <div className="h-fit">
         <ProductLinkComponent product={product} className="line-clamp-2">
           <h2>{product.name}</h2>
         </ProductLinkComponent>
-        <span className="line-clamp-2 block text-sm">by {getAuthorsByProduct(product)}</span>
+        <span className="line-clamp-2 block text-sm">
+          by {getAuthorsByProduct(product)}
+        </span>
         <span className="block text-sm my-2">ISBN: {product.isbn}</span>
-        <div className="flex gap-2">
-          <label className="inline-block cursor-pointer font-bold relative text-green-100">
-            <ChevronDownIcon className="absolute right-[11px] top-[16px]" />
-            <select className="leading-[24px] bg-green-900 p-4 pr-[40px] appearance-none cursor-pointer">
-              <option>Qty: 10</option>
-            </select>
-          </label>
-          <UiButtonComponent>Excluir</UiButtonComponent>
-        </div>
       </div>
-      <div className="col-span-2 lg:col-auto">
-        <span className="font-bold text-right block">
-          <UiPriceComponent value={20} />
+      <div className="flex gap-2 h-fit self-end col-span-2 lg:col-auto">
+        <UiButtonComponent
+          className="text-sm p-0 bg-transparent text-red-500"
+          onClick={() => {
+            cartDispatch({
+              type: CartActionTypes.REMOVE_FROM_CART,
+              payload: { id: product.id },
+            });
+          }}
+        >
+          remove
+        </UiButtonComponent>
+        <div className="border-y border-green-900 flex gap-2 items-center">
+          <UiButtonComponent
+            className="text-sm p-1.5"
+            onClick={() => {
+              cartDispatch({
+                type: CartActionTypes.DECREASE_CART_ITEM_QUANTITY,
+                payload: { id: product.id },
+              });
+            }}
+          >
+            <MinusIcon />
+          </UiButtonComponent>
+          <div className="w-10 text-center text-sm">{cartItem.quantity}</div>
+          <UiButtonComponent
+            className="text-sm p-1.5"
+            onClick={() => {
+              cartDispatch({
+                type: CartActionTypes.INCREASE_CART_ITEM_QUANTITY,
+                payload: { id: product.id },
+              });
+            }}
+          >
+            <PlusIcon />
+          </UiButtonComponent>
+        </div>
+        <span className="font-bold grow flex items-end justify-end">
+          <span className="">
+            <UiPriceComponent
+              value={cartItem.quantity * parseInt(product.price.toString())}
+            />
+          </span>
         </span>
       </div>
     </li>
