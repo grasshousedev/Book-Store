eeb2febcf17c6c922940b70dea7d38844173a57f
Sasha dev <boltbright525@gmail.com>
2024-05-08 11:52:04 -0300

    feat: redirect user to cart after added product

--- src/domains/product/components/product-page/add-to-cart-component.tsx
+++ src/domains/product/components/product-page/add-to-cart-component.tsx
@@ -6,11 +6,13 @@ import { ProductWithBookAndCategoriesPrisma } from "../../types/product-prisma";
 import { UiPriceComponent } from "@/domains/ui/components/ui-price-component";
 import { CartActionTypes } from "@/domains/cart/enums/cart-action-types";
 import { useCartContext } from "@/domains/cart/contexts/cart-context";
+import { useRouter } from "next/navigation";
 
 export function AddToCartComponent({ ...props }: React.ComponentProps<"div">) {
   const product: ProductWithBookAndCategoriesPrisma =
     useProductPageContext().state.product;
   const cartDispatch = useCartContext().dispatch;
+  const router = useRouter();
   return (
     <div {...props}>
       <div className="flex flex-col text-center border p-4">
@@ -24,6 +26,7 @@ export function AddToCartComponent({ ...props }: React.ComponentProps<"div">) {
               type: CartActionTypes.ADD_TO_CART,
               payload: { id: product.id },
             });
+            router.push('/cart');
           }}
         >
           ADD TO CART
