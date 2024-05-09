526c22fa8c9cf251deaeb399ee49d8d97dce5bfa
Sasha dev <boltbright525@gmail.com>
2024-05-09 09:41:23 -0300

    feat: empty cart

--- src/domains/cart/components/cart-page/cart-page-component.tsx
+++ src/domains/cart/components/cart-page/cart-page-component.tsx
@@ -20,41 +20,54 @@ export function CartPageComponent() {
         <main className="basis-2/3">
           <div className="p-10">
             <UiTitleComponent size="large">Shopping Cart</UiTitleComponent>
-            <p className="text-sm p-2 my-2 border bg-green-100 text-green-900 flex gap-2 items-center">
-              <InfoIcon className="shrink-0" />
-              Please verify that your shipping address and order details are
-              correct before placing your order. Orders shipping from partner
-              warehouses cannot be modified once they have begun processing.
-            </p>
-            <ul className="border-t my-4">
-              {cartItems.map((item) => (
-                <CartItemComponent key={item.id} cartItem={item} />
-              ))}
-            </ul>
-            <span className="block text-right">
-              Subtotal ({cartItemsQty} product{cartItemsQty > 1 ? "s" : ""}):{" "}
-              <span className="font-bold">
-                <UiPriceComponent value={cartItemsSubtotal.toString()} />
-              </span>
-            </span>
+            {cartItemsQty > 0 ? (
+              <>
+                <p className="text-sm p-2 my-2 border bg-green-100 text-green-900 flex gap-2 items-center">
+                  <InfoIcon className="shrink-0" />
+                  Please verify that your shipping address and order details are
+                  correct before placing your order. Orders shipping from partner
+                  warehouses cannot be modified once they have begun processing.
+                </p>
+                <ul className="border-t my-4">
+                  {cartItems.map((item) => (
+                    <CartItemComponent key={item.id} cartItem={item} />
+                  ))}
+                </ul>
+                <span className="block text-right">
+                  Subtotal ({cartItemsQty} product{cartItemsQty > 1 ? "s" : ""}):{" "}
+                  <span className="font-bold">
+                    <UiPriceComponent value={cartItemsSubtotal.toString()} />
+                  </span>
+                </span>
+              </>
+            ) : (
+              <p className="text-sm p-2 my-2 border bg-green-100 text-green-900 flex gap-2 items-center">
+                <InfoIcon className="shrink-0" />
+                There are no items in your shopping cart.
+              </p>
+            )}
           </div>
         </main>
         <aside className="basis-1/3">
           <div className="p-10">
-            <span className="block">
-              Subtotal ({cartItemsQty} product{cartItemsQty > 1 ? "s" : ""}):{" "}
-              <UiPriceComponent value={cartItemsSubtotal.toString()} />
-            </span>
-            <Link
-              href="checkout"
-              title="Checkout"
-              className="bg-green-900 font-bold p-4 text-green-100 block text-center mt-2 uppercase"
-            >
-              Checkout
-            </Link>
-            <span className="bg-green-100 p-2 block text-sm text-green-900 text-center mt-2">
-              Ship these for free.
-            </span>
+            {cartItemsQty > 0 && (
+              <>
+                <span className="block">
+                  Subtotal ({cartItemsQty} product{cartItemsQty > 1 ? "s" : ""}
+                  ): <UiPriceComponent value={cartItemsSubtotal.toString()} />
+                </span>
+                <Link
+                  href="checkout"
+                  title="Checkout"
+                  className="bg-green-900 font-bold p-4 text-green-100 block text-center mt-2 uppercase"
+                >
+                  Checkout
+                </Link>
+                <span className="bg-green-100 p-2 block text-sm text-green-900 text-center mt-2">
+                  Ship these for free.
+                </span>
+              </>
+            )}
             <Link
               href="checkout"
               title="Continue Shopping"
