44f832f8d8e696b5ea6e77fa1b8cd090a979d2ba
Sasha dev <boltbright525@gmail.com>
2024-05-15 11:43:02 -0300

    fix: allow number in slug

--- src/helpers/get-slug.ts
+++ src/helpers/get-slug.ts
@@ -1,6 +1,6 @@
 export function getSlug(text: string): string {
   return text
-    .replaceAll(/[^(a-z\s)]/gi, "")
+    .replaceAll(/[^(a-z\s0-9)]/gi, "")
     .split(" ")
     .join("-")
     .toLowerCase();
