edbf5410cb79f84593d78f39e48cb39307239a91
Sasha dev <boltbright525@gmail.com>
2024-05-20 10:21:21 -0300

    feat: warning modal

---
+++ src/domain/layout/components/warning-modal-component.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useState } from "react";
+
+export function WarningModalComponent() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [defaultOpen, setDefaultOpen] = useState(true);
+
+  return (
+    <div>
+      <Modal backdrop="blur" onClose={onClose} defaultOpen={defaultOpen}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Caution!
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  This is not real ecommerce! It is not possible to make
+                  purchases through it. This is an ecommerce for portfolio
+                  purposes only. Feel free to try: home, menu, search, product
+                  page, category page, author page, search page with filters and
+                  add to cart. I did not include a checkout process.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  I understood.
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
--- src/domain/layout/containers/layout-container.tsx
+++ src/domain/layout/containers/layout-container.tsx
@@ -5,6 +5,7 @@ import { FooterContainer } from "@/domain/layout/containers/footer-container";
 import { LayoutProvider } from "../contexts/layout-context";
 import { CategoryWithPagePrisma } from "@/domain/category/types/category-prisma";
 import { UiWrapperComponent } from "@/domain/ui/components/ui-wrapper-component";
+import { WarningModalComponent } from "../components/warning-modal-component";
 
 export async function LayoutContainer({ children }: { children: ReactNode }) {
   const categories: CategoryWithPagePrisma[] = await prisma.category.findMany({
@@ -16,6 +17,7 @@ export async function LayoutContainer({ children }: { children: ReactNode }) {
   return (
     <LayoutProvider categories={categories}>
       <div className="min-h-dvh flex flex-col">
+        <WarningModalComponent />
         <HeaderContainer />
         <UiWrapperComponent className="bg-primary-50 xl:rounded-t-lg grow -mt-20 mb-20">
           {children}
