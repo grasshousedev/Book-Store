813bdbf0ca2e52834f26ea169eccc62126301bae
Sasha dev <boltbright525@gmail.com>
2024-06-03 13:11:01 -0300

    test: configure for different screen sizes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,81 +1,51 @@
 import { defineConfig, devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./src",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? "dot" : "list",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
-      name: "mobile",
-      use: { ...devices["Pixel 7"] },
+      name: "default",
+      use: {
+        viewport: { width: 390, height: 844 },
+        isMobile: true,
+        hasTouch: true,
+      },
     },
     {
-      name: "desktop",
-      use: { ...devices["Desktop Chrome"] },
+      name: "sm", // min 640
+      use: {
+        viewport: { width: 744, height: 1133 },
+        isMobile: true,
+        hasTouch: true,
+      },
     },
     {
-      name: "tablet",
-      use: { ...devices["iPad Pro 11"] },
+      name: "md", // min 768
+      use: {
+        viewport: { width: 834, height: 1194 },
+        isMobile: true,
+        hasTouch: true,
+      },
     },
-
-    /*{
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },*/
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 12"] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+      name: "lg", // min 1024
+      use: { viewport: { width: 1024, height: 768 } },
+    },
+    {
+      name: "xl", // min 1280
+      use: { viewport: { width: 1440, height: 1024 } },
+    },
+    {
+      name: "2xl", // min 1536
+      use: { viewport: { width: 2560, height: 1080 } },
+    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
