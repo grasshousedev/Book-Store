6b14ce75c8131da3373c4609c495b065de9d8e08
Sasha dev <boltbright525@gmail.com>
2024-06-03 17:33:11 -0300

    test: author page

---
+++ src/domain/author/tests/author.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Author", () => {
+  test("should display page elements", async ({ page, isMobile }) => {
+    await page.goto("/author/donna-tartt");
+    await page.getByRole("button", { name: "I understood." }).click();
+
+    const content = page.getByTestId("content");
+
+    // Meta title
+    await expect(page).toHaveTitle("Donna Tartt's Books");
+
+    // Categories
+    if (isMobile) {
+      await expect(content.getByText("Categories")).toBeHidden();
+    } else {
+      await expect(content.getByText("Categories")).toBeVisible();
+      await expect(
+        content.getByRole("listitem").filter({ hasText: /Horror/ })
+      ).toBeVisible();
+      await expect(
+        content.getByRole("listitem").filter({ hasText: /Romance/ })
+      ).toBeVisible();
+    }
+
+    // Heading
+    await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+      "Donna Tartt"
+    );
+
+    // Product List
+    await expect(
+      content.locator("_react=ProductListItemComponent")
+    ).toHaveCount(2);
+    
+  });
+});
