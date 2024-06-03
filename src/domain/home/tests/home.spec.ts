2a50be70fab1a2b79f7ad0cc815c1f8b2f8021fd
Sasha dev <boltbright525@gmail.com>
2024-06-03 13:09:33 -0300

    test: home page

---
+++ src/domain/home/tests/home.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "I understood." }).click();
+  });
+
+  test("should display new release books", async ({ page }) => {
+    const heading = page.getByRole('heading', { name: 'New Release Books' });
+    await expect(heading).toBeVisible();
+
+    const products = page.locator("_react=ProductListItemComponent");
+    await expect(products).toHaveCount(5);
+  });
+
+  test("should display top categories", async ({ page }) => {
+    const heading = page.getByRole('heading', { name: 'Explore Our Top Categories' });
+    await expect(heading).toBeVisible();
+
+    const categories = page.locator("_react=CategoryCardComponent");
+    await expect(categories).toHaveCount(16);
+  });
+
+  test("should display featured authors", async ({ page }) => {
+    const heading = page.getByRole('heading', { name: 'Featured Authors' });
+    await expect(heading).toBeVisible();
+
+    const authors = page.locator("_react=AuthorCardComponent");
+    await expect(authors).toHaveCount(5);
+  });
+});
