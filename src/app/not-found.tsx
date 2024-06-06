89e24407b1670f497299359d5ce2f9f5d1d0d5d5
Sasha dev <boltbright525@gmail.com>
2024-06-06 15:12:43 -0300

    fix: not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import { NotFoundPageContainer } from "@/domain/layout/containers/not-found-page-container";
 
-export default async function Home() {
+export default async function NotFound() {
   return (
     <NotFoundPageContainer />
   );
