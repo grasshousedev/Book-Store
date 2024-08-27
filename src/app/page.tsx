e65764f53e49c9c6b34470e9f3f7aa555aa95cf6
Sasha dev <boltbright525@gmail.com>
2024-08-27 13:34:03 -0300

    feat: improve SEO Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import { PageType } from "@/types/page-type";
 import { handlePageBySlugAndType } from "@/helpers/handle-page-by-slug-and-type";
 import { HomePageContainer } from "@/domain/home/containers/home-page-container";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return await handlePageBySlugAndType("home", PageType.CMS_PAGE);
+export function generateMetadata(): Metadata {
+  return {
+    title: "Emmanuel's Book Store: Horror | Mystery | Romance ...",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, officia. Aperiam, quasi. Rem rerum corporis at aspernatur aperiam iusto? Fugit ipsum odit eos, repudiandae officiis id vero ipsam temporibus voluptates",
+  };
 }
 
 export default async function Home() {
