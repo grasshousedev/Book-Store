8df82267a45f48926c31f54cca36e077c2851c4c
Sasha dev <boltbright525@gmail.com>
2024-08-29 20:27:12 -0300

    feat: install Cloudinary feat: use cloudinary for categories

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+  },
+};
 
 export default nextConfig;
--- src/domain/category/components/category-card-component.tsx
+++ src/domain/category/components/category-card-component.tsx
@@ -1,7 +1,7 @@
 import { UiTitleComponent } from "@/domain/ui/components/ui-title-component";
 import { CategoryWithPagePrisma } from "../types/category-prisma";
 import { CategoryLinkComponent } from "./category-link-component";
-import Image from "next/image";
+import CldImage from "@/lib/cld-image";
 
 export function CategoryCardComponent({
   category,
@@ -12,12 +12,13 @@ export function CategoryCardComponent({
     <article className="snap-start p-5 w-[227px] h-[361px] flex-none text-sm text-pretty">
       <CategoryLinkComponent category={category}>
         <figure className="bg-green-300 w-[187px] h-[249px] overflow-hidden">
-          <Image
-            src={`/images/category/${category.page.slug}.webp`}
+          <CldImage
+            src={`category/${category.page.slug}`}
             width={187}
             height={269}
             alt={category.name}
             className="transition-all hover:-mt-[20px]"
+            crop="auto"
           />
         </figure>
       </CategoryLinkComponent>
