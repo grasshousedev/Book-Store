ef2235f868243a9a62aa2a4c7780da12e0226bbf
Sasha dev <boltbright525@gmail.com>
2024-08-30 17:29:38 -0300

    feat: add author images

--- src/domain/author/components/author-card-component.tsx
+++ src/domain/author/components/author-card-component.tsx
@@ -1,17 +1,30 @@
 import { UiTitleComponent } from "@/domain/ui/components/ui-title-component";
 import { AuthorWithPagePrisma } from "../types/author-prisma";
 import { AuthorLinkComponent } from "./author-link-component";
+import CldImage from "@/lib/cld-image";
 
-export function AuthorCardComponent({ author }: { author: AuthorWithPagePrisma }) {
+export function AuthorCardComponent({
+  author,
+}: {
+  author: AuthorWithPagePrisma;
+}) {
   return (
     <article className="p-5 w-[227px] h-[267px] flex-none text-sm text-pretty">
       <AuthorLinkComponent author={author}>
-        <div className="bg-green-300 w-[187px] h-[187px] rounded-full"></div>
+        <figure className="bg-green-300 w-[187px] h-[187px] relative overflow-hidden rounded-full">
+          <CldImage
+            src={`author/${author.page.slug}`}
+            width={207}
+            height={207}
+            alt={author.name}
+            className="transition-all w-[187px] h-[187px] hover:w-[207px] hover:h-[207px] hover:-mt-[10px] object-cover"
+            sizes="207px"
+            crop="auto"
+          />
+        </figure>
       </AuthorLinkComponent>
       <UiTitleComponent level="p" size="small" className="line-clamp-2 mt-2">
-        <AuthorLinkComponent author={author}>
-          {author.name}
-        </AuthorLinkComponent>
+        <AuthorLinkComponent author={author}>{author.name}</AuthorLinkComponent>
       </UiTitleComponent>
     </article>
   );
